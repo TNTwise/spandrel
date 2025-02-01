17f80d994b2404d0d4fd628483bb35eca84b469e
TNTwise <tntwise3@gmail.com>
2025-02-01 11:03:29 -0600

    add inference code

--- inference.py
+++ inference.py
@@ -1,6 +1,9 @@
+import torch
 from libs.spandrel.spandrel.architectures.SPANPlus.__arch.spanplus import SPANPlus
 from trthandler import TorchTensorRTHandler
 
 TorchTensorRTHandler()
 model = SPANPlus(upsampler="dys")
 model.eval().cuda()
+test = torch.rand(1, 3, 32, 32)
+model(test)
