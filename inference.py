04b8510a02dbee1ce75c0875fe7fa4cb1049dee1
TNTwise <tntwise3@gmail.com>
2025-02-01 11:18:18 -0600

    trt inference

--- inference.py
+++ inference.py
@@ -7,9 +7,10 @@ model = SPANPlus(upsampler="dys")
 test = torch.rand(1, 3, 32, 32).cuda()
 
 # inference to fix weight concat issue
+model.eval().cuda()
+
 model(test)
 
 trtHandler.build_engine(model, torch.float16, torch.device("cuda"), [test], "trt.ts")
 model = trtHandler.load_engine("trt.ts")
-model.eval().cuda()
 model(test)
