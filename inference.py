10ac8655126a6a508cad0a585efdd4b9a23f4ca4
TNTwise <tntwise3@gmail.com>
2025-02-01 11:21:35 -0600

    trt inference

--- inference.py
+++ inference.py
@@ -1,7 +1,13 @@
+import tensorrt
 import torch
+from libs.spandrel.spandrel.architectures.__arch_helpers.grid_sample_trt_plugin import (
+    WarpPluginCreator,
+)
 from libs.spandrel.spandrel.architectures.SPANPlus.__arch.spanplus import SPANPlus
 from trthandler import TorchTensorRTHandler
 
+registry = tensorrt.get_plugin_registry()
+registry.register_creator(WarpPluginCreator())
 trtHandler = TorchTensorRTHandler()
 model = SPANPlus(upsampler="dys")
 test = torch.rand(1, 3, 32, 32).cuda()
