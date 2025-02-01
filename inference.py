0d9b11edb7f9c0876b283c93ce7a0a276e236058
TNTwise <tntwise3@gmail.com>
2025-02-01 11:16:11 -0600

    trt inference

--- inference.py
+++ inference.py
@@ -2,8 +2,11 @@ import torch
 from libs.spandrel.spandrel.architectures.SPANPlus.__arch.spanplus import SPANPlus
 from trthandler import TorchTensorRTHandler
 
-TorchTensorRTHandler()
+trtHandler = TorchTensorRTHandler()
 model = SPANPlus(upsampler="dys")
-model.eval().cuda()
 test = torch.rand(1, 3, 32, 32).cuda()
+
+trtHandler.build_engine(model, torch.float16, torch.device("cuda"), [test], "trt.ts")
+model = trtHandler.load_engine("trt.ts")
+model.eval().cuda()
 model(test)
