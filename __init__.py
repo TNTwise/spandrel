f426bec4a51a24426093ea8f9505aa83ec943da4
NevermindNilas <nilascontact@gmail.com>
2025-07-03 18:44:53 +0300

    turn spandrel into a Python package

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .libs.spandrel.spandrel import (
+    ImageModelDescriptor,
+    ModelLoader,
+    UnsupportedDtypeError,
+)
+
+__all__ = [
+    "ImageModelDescriptor",
+    "ModelLoader",
+    "UnsupportedDtypeError",
+]
